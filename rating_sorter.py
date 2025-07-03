f809da0306fc5c1166c243db4bc9d4d536c539ca
Aloxiddin-05 <aloxiddinyogmirov1@gmail.com>
2025-07-03 20:09:49 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,10 @@
+import json
+from pprint import pprint
 
+with open("students.json") as jsonfile:
+    data = json.load(jsonfile)
+    
+    sortlangan = sorted(data,key=lambda x:int(x["score"]),reverse=True)
+
+with open("rating.json","w") as jsonfile:
+    json.dump(sortlangan,jsonfile,indent=4)
